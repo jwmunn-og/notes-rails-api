63ef98ead7eb0885ad01ca80476e621da358836d
jwmunn <contact@justinmunn.co>
2017-11-03 23:36:03 -0700

    Add validation for tags.

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -15,4 +15,20 @@ RSpec.describe TagsController, type: :controller do
       expect(@note.tags.first.name).to eq('Crazy')
     end
   end
+
+  describe "tags#create action validations" do
+    before do
+      note = FactoryBot.create(:note)
+      post :create, params: { tag: { name: '' }, note_id: note.id }
+    end
+
+    it "should properly deal with validation error" do
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it "should return error json on validation error" do
+      json = JSON.parse(response.body)
+      expect(json['errors']['name'][0]).to eq("can't be blank")
+    end
+  end
 end
